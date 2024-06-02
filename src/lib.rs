29d4875bc8f1e73ad9608fdd13d3b70d2f14f782
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-02 00:59:24 -0700

    impl Default for AnkiClient

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,23 @@ pub(crate) struct AnkiClient {
     version: u8,
 }
 
+impl Default for AnkiClient {
+    /// Creates a new `AnkiClient` with default values.
+    /// * `port`: The port where AnkiConnect is running. Defaults to `8765`.
+    /// * `version`: The version of the AnkiConnect plugin. Defaults to `6`.
+    /// To change these defaults, use `Ankiclient::new()` instead;
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let client = AnkiClient::default();
+    /// ```
+    fn default() -> Self {
+        Self {
+            endpoint: "http://localhost:8765".to_string(),
+            client: Client::new(),
+            version: 6,
+        }
+    }
+}
+
