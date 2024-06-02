aa3c4e87dec950a759b11a192c77408763bf79ee
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-02 00:59:14 -0700

    create AnkiClient struct

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,15 @@ mod test;
 
 use reqwest::Client;
 
+/// `AnkiClient` is a struct that allows you to communicate with the AnkiConnect API.
+///
+/// It contains the following fields:
+/// - `endpoint`: The endpoint where AnkiConnect is running. Defaults to `http://localhost:8765`.
+/// - `client`: The HTTP client used to send requests.
+/// - `version`: The version of the AnkiConnect plugin. Defaults to `6`.
+pub(crate) struct AnkiClient {
+    endpoint: String,
+    client: Client,
+    version: u8,
+}
+
