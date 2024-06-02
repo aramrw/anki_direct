4d223d5a0a12a473c915b4a8c0101b73cbf6dbe8
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-02 01:42:14 -0700

    remove mokito and tests

--- src/test.rs
+++ src/test.rs
@@ -1,35 +1,15 @@
-
 #[cfg(test)]
 mod tests {
-    use crate::AnkiClient;
     use crate::notes::NoteAction;
-    use mockito::Server;
+    use crate::AnkiClient;
 
-    #[tokio::main]
-    async fn main() {
-        test_find_notes().await;
-    }
-
+    #[tokio::test]
     async fn test_find_notes() {
-        // Request a new server from the pool
-        let mut server = Server::new();
-
-        // Create a mock
-        let mock = server
-            .mock("POST", "/")
-            .with_status(200)
-            .with_body(r#"{"result": [1483959289817, 1483959291695], "error": null}"#)
-            .create();
-
         let client = AnkiClient::default();
-        let res = NoteAction::find_note_ids(&client, "is:new").await;
+        let res = NoteAction::find_note_ids(&client, "is:new").await.unwrap();
 
         // Assert
-        assert!(res.is_ok());
-        assert_eq!(res.unwrap(), Some(vec![1483959289817, 1483959291695]));
-
-        // Verify that the mock was called
-        mock.assert();
+        let res = res.unwrap();
+        assert_eq!(res, vec![1717316234910]);
     }
 }
-
