2a02aa75c6adc309c1c741c316add23749ab0f59
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-02 01:02:08 -0700

    attempt to test Notes and fail

--- src/test.rs
+++ src/test.rs
@@ -5,3 +5,31 @@ mod tests {
     use crate::notes::NoteAction;
     use mockito::Server;
 
+    #[tokio::main]
+    async fn main() {
+        test_find_notes().await;
+    }
+
+    async fn test_find_notes() {
+        // Request a new server from the pool
+        let mut server = Server::new();
+
+        // Create a mock
+        let mock = server
+            .mock("POST", "/")
+            .with_status(200)
+            .with_body(r#"{"result": [1483959289817, 1483959291695], "error": null}"#)
+            .create();
+
+        let client = AnkiClient::default();
+        let res = NoteAction::find_note_ids(&client, "is:new").await;
+
+        // Assert
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), Some(vec![1483959289817, 1483959291695]));
+
+        // Verify that the mock was called
+        mock.assert();
+    }
+}
+
