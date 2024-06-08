9718197da9da7914d34aceb8e08fc49ccb6c56aa
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-07 17:44:29 -0700

    Error Imports

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,8 @@
+#![allow(non_snake_case)]
+use serde::{Deserialize, Serialize};
+use std::fmt::Display;
+//use std::collections::HashMap;
+
 pub fn format_error(title: &str, error: String) -> String {
     format!("{}: {}", title, error)
 }
