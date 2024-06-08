64e324e7e2177afb73c52128dffa5c96e3e62478
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-07 17:44:38 -0700

    define AnkiError enum

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,13 @@ use serde::{Deserialize, Serialize};
 use std::fmt::Display;
 //use std::collections::HashMap;
 
+#[derive(Serialize, Deserialize, Debug)]
+pub enum AnkiError {
+    NoDataFound,
+    RequestError(String),
+    ParseError(String),
+}
+
 pub fn format_error(title: &str, error: String) -> String {
     format!("{}: {}", title, error)
 }
