40a73de6c483dc0f1f7f520929ae0bd9e2e2e252
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-07 17:47:13 -0700

    test passed for fetch_note_info

--- src/test.rs
+++ src/test.rs
@@ -4,12 +4,28 @@ mod tests {
     use crate::AnkiClient;
 
     #[tokio::test]
-    async fn test_find_notes() {
+    async fn test_find_newest_notes() {
         let client = AnkiClient::default();
         let res = NoteAction::find_note_ids(&client, "is:new").await.unwrap();
 
         // Assert
-        let res = res.unwrap();
-        assert_eq!(res, vec![1717316234910]);
+        assert_eq!(*res.last().unwrap(), 1717752795958);
+    }
+
+    #[tokio::test]
+    async fn fetch_note_info() {
+        let client = AnkiClient::default();
+        let res = NoteAction::get_notes_infos(&client, vec![1717752795958])
+            .await
+            .unwrap();
+        let word = &res
+            .last()
+            .unwrap()
+            .fields
+            .get("wordDictionaryForm")
+            .unwrap()
+            .value;
+
+        assert_eq!(*word, "筒抜け");
     }
 }
